51e580d360ae498f3d2b3dc35809ed968d7824f8
Francesca <Francesca@BISANTI_ASUS>
2021-05-21 09:08:16 +0200

    Sol noleggio

--- src/main/java/polito/it/noleggio/model/Event.java
+++ src/main/java/polito/it/noleggio/model/Event.java
@@ -3,53 +3,37 @@ package polito.it.noleggio.model;
 import java.time.LocalTime;
 
 public class Event implements Comparable <Event> {
-	 //deve avere un attributo temporale, a quale istante di tempo
-	//si riferisce l'evento
-	
-	
-	public enum EventType {
-		NUOVO_CLIENTE,
-		RITORNO_AUTO
+	 private LocalTime time;
+	 
+	 public enum EventType{
+		 NUOVO_CLIENTE,
+		 RITORNO_AUTO
+		 
+	 }
+	 private EventType tipo;
+	public Event(LocalTime time, EventType tipo) {
+		super();
+		this.time = time;
+		this.tipo = tipo;
 	}
-	//le enum definiscono automaticamente delle costanti
-	//associa dei numeri agli eventi, event 1, event 2...
-	
-	
-	private LocalTime time;
-	private EventType type; //numero vincolato
+	public LocalTime getTime() {
+		return time;
+	}
+	public void setTime(LocalTime time) {
+		this.time = time;
+	}
+	public EventType getTipo() {
+		return tipo;
+	}
+	public void setTipo(EventType tipo) {
+		this.tipo = tipo;
+	}
 	
 	@Override
 	public int compareTo(Event o) {
 		
 		return this.time.compareTo(o.time);
-	} //ordino cosi gli eventi per tempo.
-
-	public Event(LocalTime time, EventType type) {
-		super();
-		this.time = time;
-		this.type = type;
 	}
-
-	public LocalTime getTime() {
-		return time;
-	}
-
-	public void setTime(LocalTime time) {
-		this.time = time;
-	}
-
-	public EventType getType() {
-		return type;
-	}
-
-	public void setType(EventType type) {
-		this.type = type;
-	}
-
-	@Override
-	public String toString() {
-		return "Event [time=" + time + ", type=" + type + "]";
-	}
-	
+	 
 	
 }
--- src/main/java/polito/it/noleggio/model/Simulator.java
+++ src/main/java/polito/it/noleggio/model/Simulator.java
@@ -7,109 +7,99 @@ import java.util.PriorityQueue;
 
 import polito.it.noleggio.model.Event.EventType;
 
+
+
 public class Simulator {
 	
 	//Eventi
-	private PriorityQueue <Event> queue;
-	
-	//parametri di simulazione
-	private int NC; //numero di macchine
-	private Duration T_IN; // intervallo di tempo tra clienti
-	private LocalTime oraApertura= LocalTime.of(8, 0);
-	
-	private LocalTime oraChiusura=LocalTime.of(20, 0);
-	
+	PriorityQueue <Event> queue;
 	
 	//Stato del sistema
-	private int nAuto; //auto attualmente presenti;
+	int nAuto;
 	
+	//parametri di simulazione
+	int NC;
+	private Duration T_IN;
+	private LocalTime oraApertura= LocalTime.of(8, 0);
+	private LocalTime oraChiusura=LocalTime.of(20, 0);
 	
-	//Misure in uscita
+	//parametri in uscita, output
 	private int nClienti;
 	private int nClientiInsoddisfatti;
 	
-	// Impostazione dei parametri iniziali
 	
-	public void setNumCars (int NC) {
-		this.NC=NC;
-		
-	}
-	
-	public void setClientFrequency (Duration d) {
-		this.T_IN=d;
-	}
-	
-	//simulazione
 	public void run() {
-		this.queue= new PriorityQueue <Event>();
+		queue= new PriorityQueue <>();
+		nAuto=NC;
+		nClienti=0;
+		nClientiInsoddisfatti=0;
+		LocalTime ora= this.oraApertura;
 		
-		
-		//Stato iniziale del sistema
-		this.nAuto=NC; //Tutte le auto sono nel garage
-		this.nClienti=0; //non ho ancora clienti
-		this.nClientiInsoddisfatti=0;
-		
-		//Eventi iniziali del sistema
-		LocalTime ora = this.oraApertura;
-		while(ora.isBefore(this.oraChiusura)) {
-			this.queue.add(new Event (ora, EventType.NUOVO_CLIENTE));
-			ora=ora.plus(this.T_IN);
+		while(ora.isBefore(oraChiusura)) {
+			queue.add(new Event(ora, EventType.NUOVO_CLIENTE));
+			ora=ora.plus(T_IN);
+			
 		}
 		
-		//Ciclo di simulazione vera e propria, sarà uguale sempre
-		while(!this.queue.isEmpty()) { //finchè la coda non è vuota
-			Event e=this.queue.poll();  //estrai il prossimo evento
+		while(!queue.isEmpty()) {
+			Event e=queue.poll();
 			processEvent(e);
 		}
 	}
-	
-	public void processEvent (Event e) {
-		switch(e.getType()) {
-		case NUOVO_CLIENTE:
-			this.nClienti++;
-			if(this.nAuto>0) {
-				
-				//noleggia
-				this.nAuto--;
-				double num= Math.random()*3; // [0,3), numero ore da agg
+
+
+	private void processEvent(Event e) {
+		switch(e.getTipo()) {
+		case NUOVO_CLIENTE: 
+			if(nAuto>0) {
+				nAuto--;
+				nClienti++;
+				double  num= Math.random()*3;
 				if(num<1.0) {
-					//prevedo che arriva un auto tra un 'ora
-					this.queue.add(new Event(e.getTime().plus(Duration.of(1, ChronoUnit.HOURS)),
-							EventType.RITORNO_AUTO)
-							); 
-					
-						
-						
-						//aggiungo un'ora con tutta quella sintassi ad "adesso"
-					
-				} else if(num<2.0) {
-					this.queue.add(new Event(e.getTime().plus(Duration.of(2, ChronoUnit.HOURS)),
-							EventType.RITORNO_AUTO)
-							); 
-				} else {
-					this.queue.add(new Event(e.getTime().plus(Duration.of(3, ChronoUnit.HOURS)),
-							EventType.RITORNO_AUTO)
-							); 
+					queue.add(new Event(e.getTime().plus(Duration.of(1, ChronoUnit.HOURS)), EventType.RITORNO_AUTO));
+				}
+				if(num<2.0) {
+					queue.add(new Event(e.getTime().plus(Duration.of(2, ChronoUnit.HOURS)), EventType.RITORNO_AUTO));
+				}
+				if(num<3.0) {
+					queue.add(new Event(e.getTime().plus(Duration.of(3, ChronoUnit.HOURS)), EventType.RITORNO_AUTO));
 				}
 				
 			}
 			else {
-				//insoddisfatto
+				this.nClienti++;
 				this.nClientiInsoddisfatti++;
 			}
 			break;
 		case RITORNO_AUTO:
-			this.nAuto++;
+			nAuto++;
 			break;
 		}
+		
 	}
-	
+
+
+	public void setNumCars(int NC) {
+		this.NC=NC;
+		
+		
+	}
+
+
+	public void setClientFrequency(Duration of) {
+		this.T_IN=of;
+		
+	}
+
+
 	public int getTotClients() {
 		return this.nClienti;
 	}
+
+
 	public int getDissatisfied() {
+		
 		return this.nClientiInsoddisfatti;
 	}
-	
 
 }
